b6d431df4926cbe1e324a811478ec47036166375
kaka888970 <zoujiawei950@sina.com>
2020-04-02 21:50:27 +0800

    Initial commit

---
+++ eureka8080/src/main/java/com/doublez/eureka8080/Eureka8080Application.java
@@ -0,0 +1,13 @@
+package com.doublez.eureka8080;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka8080Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka8080Application.class, args);
+    }
+
+}
---
+++ eureka8080/src/test/java/com/doublez/eureka8080/Eureka8080ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doublez.eureka8080;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka8080ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
